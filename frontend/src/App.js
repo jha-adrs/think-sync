b6d286f8a5213d2c289f49a6ba1768d98c69fd9f
Aadarsh Jha <aadarshjha6783@gmail.com>
2023-08-03 17:49:16 +0530

    before using context API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+    </BrowserRouter>
+
   );
 }
 
